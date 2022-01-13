6b7137ef0a4753f4eefe61bab8a19ac640969272
tarjanb <tarjan.b@gmail.com>
2022-01-13 16:56:39 +0100

    update neural rescorer script

--- scripts/asr_language_modeling/neural_rescorer/eval_neural_rescorer.sh
+++ scripts/asr_language_modeling/neural_rescorer/eval_neural_rescorer.sh
@@ -1,6 +1,26 @@
 
-python eval_neural_rescorer.py \
-    --lm_model NYTK/text-generation-news-gpt2-small-hungarian \
-    --beams_file ../ngram_lm/results/preds/train-114__spok_norm_10-1_obh_postproc__ip061_3gram__dev-spont-indep/preds_out_width80_alpha1.6_beta0.0.tsv \
-    --beam_size 80 \
-    --eval_manifest /data/BEA-1/dev-spont-indep/dev-spont-indep.json
+
+neural_rescore()
+{
+  python eval_neural_rescorer.py \
+    --lm_model "${NEURAL_LM}" \
+    --beams_file ../ngram_lm/results/preds/${MODEL_AND_TEST}/${PREDS_CONFIG}.tsv \
+    --beam_size "${BEAM_SIZE}" \
+    --eval_manifest "${TEST}" \
+    --scores_output_file ${MODEL_AND_TEST}.tsv \
+    > ${MODEL_AND_TEST}.log
+}
+
+# Test sets
+test_dev_spont=/data/BEA-1/dev-spont-indep/dev-spont-indep.json
+test_dev_planned=/data/BEA-1/dev-planned-indep/dev-planned-indep.json
+test_eval_spont=/data/BEA-1/eval-spont-indep/eval-spont-indep16.json
+test_eval_planned=/data/BEA-1/eval-planned-indep/eval-planned-indep16.json
+
+NEURAL_LM=NYTK/text-generation-news-gpt2-small-hungarian
+BEAM_SIZE=80
+TEST=${test_eval_spont}
+MODEL_AND_TEST=train-114_3gram__eval-spont-indep16
+PREDS_CONFIG=preds_out_width80_alpha2.2_beta-0.5
+
+neural_rescore
