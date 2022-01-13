7ef72519bbb22f27600904085a2e1d22dc9fb4a2
tarjanb <tarjan.b@gmail.com>
2022-01-13 20:38:38 +0100

    update neural rescorer

--- scripts/asr_language_modeling/neural_rescorer/eval_neural_rescorer.sh
+++ scripts/asr_language_modeling/neural_rescorer/eval_neural_rescorer.sh
@@ -16,11 +16,13 @@ test_dev_spont=/data/BEA-1/dev-spont-indep/dev-spont-indep.json
 test_dev_planned=/data/BEA-1/dev-planned-indep/dev-planned-indep.json
 test_eval_spont=/data/BEA-1/eval-spont-indep/eval-spont-indep16.json
 test_eval_planned=/data/BEA-1/eval-planned-indep/eval-planned-indep16.json
+test_dev_spont_no_empty=../manifests/dev-spont-indep-no_empty.json
+test_eval_spont_no_empty=../manifests/eval-spont-indep16-no_empty.json
 
 NEURAL_LM=NYTK/text-generation-news-gpt2-small-hungarian
 BEAM_SIZE=80
-TEST=${test_eval_spont}
-MODEL_AND_TEST=train-114_3gram__eval-spont-indep16
+TEST=${test_dev_spont_no_empty}
+MODEL_AND_TEST=train-114_3gram__dev-spont-indep-no_empty
 PREDS_CONFIG=preds_out_width80_alpha2.2_beta-0.5
 
 neural_rescore
