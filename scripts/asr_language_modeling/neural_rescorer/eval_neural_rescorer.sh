953bfd83523f32387ede6e2cf834b4544ed6f5de
tarjanb <tarjan.b@gmail.com>
2022-01-14 11:54:07 +0100

    update neural rescorer script

--- scripts/asr_language_modeling/neural_rescorer/eval_neural_rescorer.sh
+++ scripts/asr_language_modeling/neural_rescorer/eval_neural_rescorer.sh
@@ -1,14 +1,26 @@
-
+#!/usr/bin/env bash
 
 neural_rescore()
 {
-  python eval_neural_rescorer.py \
-    --lm_model "${NEURAL_LM}" \
-    --beams_file ../ngram_lm/results/preds/${MODEL_AND_TEST}/${PREDS_CONFIG}.tsv \
-    --beam_size "${BEAM_SIZE}" \
-    --eval_manifest "${TEST}" \
-    --scores_output_file ${MODEL_AND_TEST}.tsv \
-    > ${MODEL_AND_TEST}.log
+  if [ -z "${ALPHA}" ] && [ -z "${BETA}" ]; then
+    python eval_neural_rescorer.py \
+      --lm_model "${NEURAL_LM}" \
+      --beams_file ../ngram_lm/results/preds/${MODEL_AND_TEST}/${PREDS_CONFIG}.tsv \
+      --beam_size "${BEAM_SIZE}" \
+      --eval_manifest "${TEST}" \
+      --scores_output_file ${MODEL_AND_TEST}.tsv \
+      > ${MODEL_AND_TEST}.log
+  else
+    python eval_neural_rescorer.py \
+      --lm_model "${NEURAL_LM}" \
+      --beams_file ../ngram_lm/results/preds/${MODEL_AND_TEST}/${PREDS_CONFIG}.tsv \
+      --beam_size "${BEAM_SIZE}" \
+      --eval_manifest "${TEST}" \
+      --alpha ${ALPHA} \
+      --beta ${BETA} \
+      --scores_output_file ${MODEL_AND_TEST}_manual.tsv \
+      > ${MODEL_AND_TEST}_manual.log
+  fi
 }
 
 # Test sets
@@ -21,8 +33,21 @@ test_eval_spont_no_empty=../manifests/eval-spont-indep16-no_empty.json
 
 NEURAL_LM=NYTK/text-generation-news-gpt2-small-hungarian
 BEAM_SIZE=80
+
+#############
 TEST=${test_dev_spont_no_empty}
-MODEL_AND_TEST=train-114_3gram__dev-spont-indep-no_empty
-PREDS_CONFIG=preds_out_width80_alpha2.2_beta-0.5
+MODEL_AND_TEST=train-114__spok_norm_10-1_obh_postproc__ip061_3gram__dev-spont-indep-no_empty
+PREDS_CONFIG=preds_out_width80_alpha1.6_beta0.0.tsv
+ALPHA=
+BETA=
+
+neural_rescore
+
+#############
+TEST=${test_eval_spont_no_empty}
+MODEL_AND_TEST=train-114__spok_norm_10-1_obh_postproc__ip061_3gram__eval-spont-indep16-no_empty
+PREDS_CONFIG=preds_out_width80_alpha1.6_beta0.0.tsv
+ALPHA=
+BETA=
 
 neural_rescore
