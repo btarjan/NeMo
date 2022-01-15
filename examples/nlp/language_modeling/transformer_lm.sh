9304c739498875896d627dab94ad1c332b263357
tarjanb <tarjan.b@gmail.com>
2022-01-15 16:17:44 +0100

    update transformer lm training scripts to repo

--- examples/nlp/language_modeling/transformer_lm.sh
+++ examples/nlp/language_modeling/transformer_lm.sh
@@ -5,6 +5,8 @@ mkdir -p results
 python transformer_lm.py \
   -cn transformer_lm_config \
   trainer.gpus=2 \
+  trainer.max_epochs=10 \
+  model.train_ds.tokens_in_batch=16384 \
   +exp_manager.exp_dir=./results \
   +exp_manager.create_tensorboard_logger=True \
   +exp_manager.create_checkpoint_callback=True \
@@ -16,5 +18,5 @@ python transformer_lm.py \
   model.test_ds.file_name=txts/eval-spont-indep.txt \
   model.tokenizer.tokenizer_model=tokenizers/spok_norm_10-1_obh_postproc_30k \
   +model.head.use_transformer_init=True \
-  +trainer.val_check_interval=0.1
+  +trainer.val_check_interval=0.1 \
 
