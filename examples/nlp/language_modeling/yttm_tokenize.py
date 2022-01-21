67856b0c7b2dc5cf6ac36b92acc2f95b4a3a43fa
tarjanb <tarjan.b@gmail.com>
2022-01-21 16:55:57 +0100

    update tokenizing script

--- examples/nlp/language_modeling/yttm_tokenize.py
+++ examples/nlp/language_modeling/yttm_tokenize.py
@@ -1,7 +1,12 @@
 
 import youtokentome as yttm
 
-data = "txts/spok_norm_10-1_obh_postproc.txt"
-model = "tokenizers/spok_norm_10-1_obh_postproc_30k"
-vocab_size = 30000
+# data = "txts/spok_norm_10-1_obh_postproc.txt"
+# model = "tokenizers/spok_norm_10-1_obh_postproc_30k"
+# vocab_size = 30000
+# yttm.BPE.train(data, model, vocab_size)
+
+data = "txts/train-114.txt"
+model = "tokenizers/train-114_10k"
+vocab_size = 10000
 yttm.BPE.train(data, model, vocab_size)
