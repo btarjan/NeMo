8ad53040f0494075874afcee0c28c44fad660ee0
tarjanb <tarjan.b@gmail.com>
2022-01-23 16:52:10 +0100

    update ckpt to nemo script

--- examples/nlp/language_modeling/save_transformer_lm_to_nemo.py
+++ examples/nlp/language_modeling/save_transformer_lm_to_nemo.py
@@ -1,10 +1,10 @@
 
 from nemo.collections.nlp.models.language_modeling import TransformerLMModel
 
-model = TransformerLMModel.load_from_checkpoint(
-    checkpoint_path="results/TransformerLM_PT_spok/2022-01-21_10-52-36/checkpoints/TransformerLM_PT_spok--val_PPL=524.5120-epoch=7.ckpt",
-    hparams_file="results/TransformerLM_PT_spok/2022-01-21_10-52-36/hparams.yaml",
-    map_location=None,
-)
 
-model.save_to("results/TransformerLM_PT_spok/2022-01-21_10-52-36/checkpoints/TransformerLM_PT_spok--val_PPL=524.5120-epoch=7.nemo")
+def cpkt_to_nemo(checkpoint_path: str):
+    model = TransformerLMModel.load_from_checkpoint(checkpoint_path=checkpoint_path)
+    model.save_to(checkpoint_path.replace("ckpt", "nemo"))
+
+
+cpkt_to_nemo("results/TransformerLM_FT_train-114/2022-01-22_18-16-21/checkpoints/TransformerLM_FT_train-114--val_PPL=202.8741-epoch=23.ckpt")
