6faf03d12004570dda48df558bcb5828c1b08411
tarjanb <tarjan.b@gmail.com>
2022-02-02 17:06:07 +0100

    add new run scripts

---
+++ examples/asr/asr_ctc/train_citrinet_ctc_bpe.sh
@@ -0,0 +1,13 @@
+python ./speech_to_text_ctc.py \
+    --config-path=../conf/citrinet \
+    --config-name=config_bpe \
+    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
+    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
+    model.tokenizer.dir="/home/tarjanb/NeMo/examples/asr/asr_ctc/tokenizers/tokenizer_spe_unigram_v128/" \
+    model.tokenizer.type="bpe" \
+    hydra.run.dir="." \
+    trainer.gpus=2 \
+    trainer.max_epochs=100 \
+    model.train_ds.batch_size=16
+
+
