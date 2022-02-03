fe30e16f8bc4b10a30bfac8f8ddfbea2546f5bec
tarjanb <tarjan.b@gmail.com>
2022-02-03 18:05:44 +0100

    add conformer scripts to repo

---
+++ examples/asr/train_conformer_ctc.sh
@@ -0,0 +1,12 @@
+python ./speech_to_text.py \
+    --config-path=conf/conformer \
+    --config-name=conformer_ctc_char_hu \
+    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
+    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
+    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
+    hydra.run.dir="." \
+    trainer.gpus=2 \
+    trainer.max_epochs=100 \
+    model.train_ds.batch_size=32 \
+    model.validation_ds.batch_size=4 \
+    model.test_ds.batch_size=4
---
+++ examples/asr/train_conformer_ctc_bpe.sh
@@ -0,0 +1,14 @@
+python ./speech_to_text_bpe.py \
+    --config-path=conf/conformer \
+    --config-name=conformer_ctc_bpe_medium \
+    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
+    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
+    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
+    model.tokenizer.dir="tokenizers/tokenizer_spe_unigram_v128" \
+    model.tokenizer.type="bpe" \
+    hydra.run.dir="." \
+    trainer.gpus=2 \
+    trainer.max_epochs=100 \
+    model.train_ds.batch_size=32
+
+
