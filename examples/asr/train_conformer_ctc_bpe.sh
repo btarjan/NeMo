1cfd56f16905fbda1bf36d385931c64b831db4e9
tarjanb <tarjan.b@gmail.com>
2022-02-07 11:11:33 +0100

    update conformer training script

--- examples/asr/train_conformer_ctc_bpe.sh
+++ examples/asr/train_conformer_ctc_bpe.sh
@@ -1,3 +1,5 @@
+
+## Medium from scratch
 #python ./speech_to_text_bpe.py \
 #    --config-path=conf/conformer \
 #    --config-name=conformer_ctc_bpe_medium \
@@ -11,9 +13,26 @@
 #    trainer.max_epochs=100 \
 #    model.train_ds.batch_size=32
 
+## Medium from pretrained
+#python ./speech_to_text_bpe.py \
+#    --config-path=conf/conformer \
+#    --config-name=conformer_ctc_bpe_medium \
+#    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
+#    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
+#    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
+#    model.tokenizer.dir="tokenizers/tokenizer_spe_unigram_v128" \
+#    model.tokenizer.type="bpe" \
+#    hydra.run.dir="." \
+#    trainer.gpus=2 \
+#    trainer.max_epochs=100 \
+#    model.train_ds.batch_size=32 \
+#    model.optim.lr=2.0 \
+#    +init_from_pretrained_model="stt_en_conformer_ctc_medium"
+
+## Large from pretrained
 python ./speech_to_text_bpe.py \
     --config-path=conf/conformer \
-    --config-name=conformer_ctc_bpe_medium \
+    --config-name=conformer_ctc_bpe \
     model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
     model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
     model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
@@ -22,6 +41,6 @@ python ./speech_to_text_bpe.py \
     hydra.run.dir="." \
     trainer.gpus=2 \
     trainer.max_epochs=100 \
-    model.train_ds.batch_size=32 \
-    model.optim.lr=0.5 \
-    +init_from_pretrained_model="stt_en_conformer_ctc_medium"
+    model.train_ds.batch_size=8 \
+    model.optim.lr=1.0 \
+    +init_from_pretrained_model="stt_en_conformer_ctc_large"
