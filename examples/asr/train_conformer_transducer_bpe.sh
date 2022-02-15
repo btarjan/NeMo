ee9eddfe56efdecb75ecaa15b49c487b7825e185
tarjanb <tarjan.b@gmail.com>
2022-02-15 21:37:32 +0100

    update conformer transformer script

--- examples/asr/train_conformer_transducer_bpe.sh
+++ examples/asr/train_conformer_transducer_bpe.sh
@@ -15,9 +15,26 @@
 #    model.optim.lr=2.0 \
 
 ## Medium from pretrained
+#python ./speech_to_text_rnnt_bpe.py \
+#    --config-path=conf/conformer \
+#    --config-name=conformer_transducer_bpe_medium \
+#    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
+#    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
+#    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
+#    model.tokenizer.dir="tokenizers/tokenizer_spe_unigram_v1024" \
+#    model.tokenizer.type="bpe" \
+#    hydra.run.dir="." \
+#    trainer.gpus=2 \
+#    trainer.max_epochs=100 \
+#    model.train_ds.batch_size=16 \
+#    model.optim.lr=2.0 \
+#    +init_from_pretrained_model="stt_en_conformer_transducer_medium" \
+#    trainer.accumulate_grad_batches=2
+
+## Large from pretrained
 python ./speech_to_text_rnnt_bpe.py \
     --config-path=conf/conformer \
-    --config-name=conformer_transducer_bpe_medium \
+    --config-name=conformer_transducer_bpe \
     model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
     model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
     model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
@@ -26,24 +43,7 @@ python ./speech_to_text_rnnt_bpe.py \
     hydra.run.dir="." \
     trainer.gpus=2 \
     trainer.max_epochs=100 \
-    model.train_ds.batch_size=16 \
-    model.optim.lr=1.0 \
-    +init_from_pretrained_model="stt_en_conformer_transducer_medium" \
-    trainer.accumulate_grad_batches=2
-
-## Large from pretrained
-#python ./speech_to_text_bpe.py \
-#    --config-path=conf/conformer \
-#    --config-name=conformer_ctc_bpe \
-#    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
-#    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
-#    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
-#    model.tokenizer.dir="tokenizers/tokenizer_spe_unigram_v128" \
-#    model.tokenizer.type="bpe" \
-#    hydra.run.dir="." \
-#    trainer.gpus=2 \
-#    trainer.max_epochs=50 \
-#    model.train_ds.batch_size=8 \
-#    model.optim.lr=1.0 \
-#    +init_from_pretrained_model="stt_en_conformer_ctc_large" \
-#    trainer.accumulate_grad_batches=4
+    model.train_ds.batch_size=8 \
+    model.optim.lr=2.0 \
+    +init_from_pretrained_model="stt_en_conformer_transducer_large" \
+    trainer.accumulate_grad_batches=4
