0b850c28887c74567a8e5199b5d1aaf4ceb58cdf
tarjanb <tarjan.b@gmail.com>
2022-02-18 14:47:06 +0100

    update citrinet training script

--- examples/asr/train_citrinet.sh
+++ examples/asr/train_citrinet.sh
@@ -1,26 +1,8 @@
 
 ## 512 from pretrained
-python ./speech_to_text_bpe.py \
-    --config-path=conf/citrinet \
-    --config-name=citrinet_512 \
-    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
-    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
-    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
-    model.tokenizer.dir="tokenizers/tokenizer_spe_unigram_v1024" \
-    model.tokenizer.type="bpe" \
-    hydra.run.dir="." \
-    trainer.gpus=2 \
-    trainer.max_epochs=100 \
-    model.train_ds.batch_size=32 \
-    +init_from_pretrained_model="stt_en_citrinet_512"
-
-    #model.optim.lr=2.0 \
-    #trainer.accumulate_grad_batches=2
-
-## Large from pretrained
-#python ./speech_to_text_rnnt_bpe.py \
-#    --config-path=conf/conformer \
-#    --config-name=conformer_transducer_bpe \
+#python ./speech_to_text_bpe.py \
+#    --config-path=conf/citrinet \
+#    --config-name=citrinet_512 \
 #    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
 #    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
 #    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
@@ -29,7 +11,23 @@ python ./speech_to_text_bpe.py \
 #    hydra.run.dir="." \
 #    trainer.gpus=2 \
 #    trainer.max_epochs=100 \
-#    model.train_ds.batch_size=8 \
-#    model.optim.lr=2.0 \
-#    +init_from_pretrained_model="stt_en_conformer_transducer_large" \
-#    trainer.accumulate_grad_batches=4
+#    model.optim.lr=0.01 \
+#    model.train_ds.batch_size=32 \
+#    +init_from_pretrained_model="stt_en_citrinet_512"
+
+
+## 1024 from pretrained
+python ./speech_to_text_bpe.py \
+    --config-path=conf/citrinet \
+    --config-name=citrinet_1024 \
+    model.train_ds.manifest_filepath="/data/BEA-Base.json/train-114.json" \
+    model.validation_ds.manifest_filepath="/data/BEA-Base.json/dev-spont.json" \
+    model.test_ds.manifest_filepath="/data/BEA-Base.json/eval-spont.json" \
+    model.tokenizer.dir="tokenizers/tokenizer_spe_unigram_v1024" \
+    model.tokenizer.type="bpe" \
+    hydra.run.dir="." \
+    trainer.gpus=2 \
+    trainer.max_epochs=100 \
+    model.optim.lr=0.01 \
+    model.train_ds.batch_size=32 \
+    +init_from_pretrained_model="stt_en_citrinet_1024"
